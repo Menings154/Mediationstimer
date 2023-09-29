a6e24c3d5ec43ea08bd23355ef4f85f99f5f99cc
Menings <menings.git@gmail.com>
2023-09-29 07:27:34 +0200

    Initial commit

---
+++ Meditationstimer/Meditationstimer.ino
@@ -0,0 +1,19 @@
+#define TimerMin 10 
+
+#define Pgreen 36
+#define Pred 39
+#define Pbuzzer 41
+#define Pbutton 40
+
+void setup() {
+  pinMode(Pgreen, OUTPUT);
+  pinMode(Pred, OUTPUT);
+  pinMode(Pbuzzer, OUTPUT);
+  pinMode(Pbutton, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
