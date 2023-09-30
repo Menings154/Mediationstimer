9f0b5ee6649bc9d12b99da63e2efb92c68c9b1bd
Menings <menings.git@gmail.com>
2023-09-30 09:41:31 +0200

    Test to get back into coding with an arduino/esp32

--- Meditationstimer/Meditationstimer.ino
+++ Meditationstimer/Meditationstimer.ino
@@ -1,9 +1,15 @@
-#define TimerMin 10 
+#define Pgreen 23
+#define Pred 22
+#define Pbuzzer 19
+#define Pbutton 18
 
-#define Pgreen 36
-#define Pred 39
-#define Pbuzzer 41
-#define Pbutton 40
+int TimerMin = 10;
+int state = 0;
+int oldstate = 0;
+bool pressed = false;
+bool timing = false;
+int ende;
+
 
 void setup() {
   pinMode(Pgreen, OUTPUT);
@@ -11,9 +17,66 @@ void setup() {
   pinMode(Pbuzzer, OUTPUT);
   pinMode(Pbutton, INPUT_PULLUP);
 
+  digitalWrite(Pbuzzer, LOW);
+
+  Serial.begin(9600);
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!pressed)
+  {
+    if (digitalRead(Pbutton) == LOW)
+    {
+      oldstate = state;
+      pressed = true;
+      state += 1;
+      state = state % 3;
+      Serial.println(state);
+    }
+    
+    if (state == 1)
+    {
+      digitalWrite(Pgreen, HIGH);
+      digitalWrite(Pred, LOW);
+      if (oldstate != state)
+      {
+        ende = millis() + TimerMin*60*1000;
+        timing = true;
+      }
+      if (millis()>=ende and timing==true);
+      {
+        state = 4;
+      }
+    }
+    if (state == 2)
+    {
+      digitalWrite(Pred, HIGH);
+      digitalWrite(Pgreen, LOW);
+    }
+    if (state == 0)
+    {
+      digitalWrite(Pred, LOW);
+      digitalWrite(Pgreen, LOW);
+    }
+  }
 
+  if (pressed)
+  {
+    if (digitalRead(Pbutton) == HIGH)
+    {
+      pressed = false;
+    }
+  }
+
+  if (state == 4)
+  {
+    digitalWrite(Pbuzzer, HIGH);
+    if (digitalRead(Pbutton) == LOW)
+    {
+      timing = false;
+      digitalWrite(Pbuzzer, LOW);
+      state = 1;
+    }
+  }
 }
